cfe7da648011082c5d90b9176e6a8d81d4eba464
manbhav234 <manbhav234@gmail.com>
2023-09-13 16:24:27 +0530

    Added localStorage for creating todo and updating complete status

--- scripts/app.js
+++ scripts/app.js
@@ -6,8 +6,9 @@ const overlay = document.querySelector('#overlay') // Background dark on overlay
 const createOverlay = document.querySelector('.create-todo-overlay') // Main create todo overlay
 const createTodoForm = document.querySelector('.create-todo-form') // Create todo form
 
+
 // Javascript for creating the to-do html
-const createTodoHTML = function (titleText, deadline){
+const createTodoHTML = function (titleText, deadline, isComplete, index){
     const mainDiv = document.createElement('div')
     const checkBoxInput = document.createElement('input')
     const contentDiv = document.createElement('div')
@@ -21,6 +22,7 @@ const createTodoHTML = function (titleText, deadline){
     
     checkBoxInput.type = 'checkbox'
     checkBoxInput.classList.add('checkbox')
+    checkBoxInput.id = `${index}`
     mainDiv.appendChild(checkBoxInput)
     
     contentDiv.classList.add('todo-content')
@@ -40,9 +42,55 @@ const createTodoHTML = function (titleText, deadline){
     deleteImg.classList.add('delete')
     mainDiv.appendChild(deleteImg)
 
+    if (isComplete){
+        checkBoxInput.setAttribute('checked', 'checked')
+        checkBoxInput.nextElementSibling.firstElementChild.style.textDecoration = 'line-through'
+        checkBoxInput.nextElementSibling.firstElementChild.nextElementSibling.style.textDecoration = 'line-through'
+    }
+
     toDoContainer.appendChild(mainDiv)
 }
 
+// Fetching and displaying todos from the local storage
+if(localStorage.getItem('todoList')){
+    let todoList = JSON.parse(localStorage.getItem('todoList'))
+
+    todoList.forEach((value, index) => {
+        createTodoHTML(value.title, value.deadline, value.isComplete, index)
+    })
+}
+
+//Add a todo to local storage
+const addToLocalStorage = function (title,description, deadline){
+    let newTodoObj = {
+        title: title,
+        description: description,
+        deadline: deadline,
+        isComplete: false
+    }
+    if (!localStorage.getItem('todoList')){
+        let todoList = []
+        todoList.push(newTodoObj)
+        localStorage.setItem('todoList', JSON.stringify(todoList))
+    }
+    else{
+        let todoList = JSON.parse(localStorage.getItem('todoList'))
+        todoList.push(newTodoObj)
+        localStorage.setItem('todoList', JSON.stringify(todoList))
+    }
+}
+
+const updateCheckLocalStorage = function(setVal, elemId) {
+    let todoList = JSON.parse(localStorage.getItem('todoList'))
+    todoList.forEach((value, index) => {
+        if(Number(elemId) === index){
+            value.isComplete = setVal;
+        }
+    })
+    localStorage.setItem('todoList', JSON.stringify(todoList))
+}
+
+// Delete and checkbox click listeners
 toDoContainer.addEventListener('click', function(e){
 
     //Delete Button
@@ -56,17 +104,19 @@ toDoContainer.addEventListener('click', function(e){
             e.target.setAttribute('checked', 'checked')
             e.target.nextElementSibling.firstElementChild.style.textDecoration = 'line-through'
             e.target.nextElementSibling.firstElementChild.nextElementSibling.style.textDecoration = 'line-through'
+            updateCheckLocalStorage(true,e.target.id)
 
         }else {
             e.target.removeAttribute('checked')
             e.target.nextElementSibling.firstElementChild.style.textDecoration = 'none'
             e.target.nextElementSibling.firstElementChild.nextElementSibling.style.textDecoration = 'none'
+            updateCheckLocalStorage(false,e.target.id)
         }
     }
 
 })
 
-// Main Create new todo btn
+// Main Create new todo buttonn click listener
 createToDoBtn.addEventListener('click', function(e){
     if(!(createOverlay.classList.contains('active'))){
         createOverlay.classList.add('active')
@@ -86,6 +136,7 @@ createTodoForm.addEventListener('submit', function(e){
     const createTodoDeadline = document.querySelector('#create-deadline').value
     createTodoHTML(createTodoTitle, createTodoDeadline)
     createTodoForm.reset()
+    addToLocalStorage(createTodoTitle, createTodoDesc, createTodoDeadline)
     createOverlay.classList.remove('active')
     overlay.classList.remove('active')
 })
